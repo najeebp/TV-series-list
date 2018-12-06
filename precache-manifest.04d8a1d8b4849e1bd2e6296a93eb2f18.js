cb91e27bb9a5bc31c9314276d655899a58ea6dd1
Najeeb <najeeb.p@mobme.in>
2018-12-06 18:08:10 +0530

    Updates

---
+++ precache-manifest.04d8a1d8b4849e1bd2e6296a93eb2f18.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/TV-series-list/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "f845d6e9bfa389a3f837",
+    "url": "/TV-series-list/static/css/main.82932281.chunk.css"
+  },
+  {
+    "revision": "faca6eab75cbfbde55d0",
+    "url": "/TV-series-list/static/js/1.faca6eab.chunk.js"
+  },
+  {
+    "revision": "0f565801e9b5f8e844c1",
+    "url": "/TV-series-list/static/js/runtime~main.0f565801.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/TV-series-list/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/TV-series-list/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/TV-series-list/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "f845d6e9bfa389a3f837",
+    "url": "/TV-series-list/static/js/main.f845d6e9.chunk.js"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/TV-series-list/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/TV-series-list/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/TV-series-list/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/TV-series-list/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/TV-series-list/static/media/poster9.65d6d500.jpg"
+  },
+  {
+    "revision": "2c920fd8210f82acfa1e41764d4c148f",
+    "url": "/TV-series-list/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/TV-series-list/precache-manifest.04d8a1d8b4849e1bd2e6296a93eb2f18.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/TV-series-list/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
